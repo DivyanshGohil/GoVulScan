8d601d468c71faf4c4b3a40168c1f862b7b430ea
Gohil Divyansh <divyansh.gohil@gmail.com>
2025-04-17 18:09:27 +0530

    Init the Go main

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/DivyanshGohil/GoVulScan
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Network Vulnerability Scanner Written in Go")
+}
