dbe86bd3fbded6f9177ca67df930f751cc77e5c0
Gohil Divyansh <divyansh.gohil@gmail.com>
2025-04-19 16:46:51 +0530

    Simple Port Scanner Code

--- main.go
+++ main.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"time"
 )
 
+func scanPort(host string, port int, timeout time.Duration) bool {
+	target := fmt.Sprintf("%s:%d", host, port)
+	conn, err := net.DialTimeout("tcp", target, timeout)
+
+	if err != nil {
+		return false
+	}
+
+	conn.Close()
+	return true
+}
+
 func main() {
-	fmt.Println("Network Vulnerability Scanner Written in Go")
+
+	host := "localhost"
+	timeout := time.Second * 2
+	fmt.Printf("Scanning host: %s\n", host)
+
+	//scan the first 1024 ports
+	for port := 1; port <= 1024; port++ {
+		if scanPort(host, port, timeout) {
+			fmt.Printf("Port %d is open\n", port)
+		}
+	}
+
+	fmt.Println("Scan complete")
 }
